6e3cf8de7d063511a6aef1f93bb74846e3028ca3
Annie Glerum <aglerum@users.noreply.github.com>
2020-10-20 17:11:49 -0400

    Update ISBN_eBooks.sql

--- GOBI_ISBN_reports/SQL/ISBN_eBooks.sql
+++ GOBI_ISBN_reports/SQL/ISBN_eBooks.sql
@@ -4,7 +4,7 @@ LEFT JOIN sb.al_items ON sb.al_items.bib_doc_num = sb.al_isbn.bib_doc_num
 LEFT JOIN sb.al_bib_fmt ON sb.al_bib_fmt.bib_doc_num = sb.al_isbn.bib_doc_num
 WHERE sb.al_isbn.scode = 'a'
 AND sb.al_items.inst = 'FS'
-AND sb.al_items.sub_lib = 'FSUER'
+AND (sb.al_items.sub_lib = 'FSUER' OR sb.al_items.sub_lib = 'FSOAR')
 AND (sb.al_items.ips != 'WD' 
     OR sb.al_items.ips != 'VC' 
     OR sb.al_items.ips != 'MI' 
