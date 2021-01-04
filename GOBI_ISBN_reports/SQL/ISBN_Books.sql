cf7dbaed46dd06da6876b16ba27c8d76376ccc1c
Annie Glerum <aglerum@users.noreply.github.com>
2021-01-04 12:05:39 -0500

    Update ISBN_Books.sql

--- GOBI_ISBN_reports/SQL/ISBN_Books.sql
+++ GOBI_ISBN_reports/SQL/ISBN_Books.sql
@@ -5,6 +5,7 @@ LEFT JOIN sb.al_bib_fmt ON sb.al_bib_fmt.bib_doc_num = sb.al_isbn.bib_doc_num
 WHERE sb.al_isbn.scode = 'a'
 AND sb.al_items.inst = 'FS'
 AND (sb.al_items.sub_lib != 'FSUER' 
+    AND sb.al_items.sub_lib != 'FSOAR' 
     AND sb.al_items.sub_lib != 'FSUDG' 
     AND sb.al_items.sub_lib != 'FSUMC')
 AND (sb.al_items.ips != 'WD' 
@@ -20,11 +21,8 @@ AND (sb.al_items.ips != 'WD'
     OR sb.al_items.ips != 'CO' 
     OR sb.al_items.ips != 'CA' 
     OR sb.al_items.ips != 'AC')
-AND (sb.al_items.open_date LIKE '%AUG-18' 
-    OR sb.al_items.open_date LIKE '%SEP-18' 
+AND (
     OR sb.al_items.open_date LIKE '%OCT-18' 
     OR sb.al_items.open_date LIKE '%NOV-18' 
-    OR sb.al_items.open_date LIKE '%DEC-18' 
-    OR sb.al_items.open_date LIKE '%19' 
-    OR sb.al_items.open_date LIKE '%20')
+    OR sb.al_items.open_date LIKE '%DEC-18')
 AND sb.al_bib_fmt.bib_fmt = 'BK';
