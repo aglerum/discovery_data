ac56ed65c556aaf2c4de8ff7714cf6c644104c6d
Annie Glerum <aglerum@users.noreply.github.com>
2021-06-29 13:11:48 -0400

    Update ISBN_eBooks.sql

--- GOBI_ISBN_reports/SQL/ISBN_eBooks.sql
+++ GOBI_ISBN_reports/SQL/ISBN_eBooks.sql
@@ -1,7 +1,7 @@
 SELECT sb.al_isbn.bib_doc_num BIB, sb.al_isbn.isbn, sb.al_isbn.paren, sb.al_items.sub_lib, sb.al_items.coll_code, sb.al_items.open_date, sb.al_items.ips
 FROM sb.al_isbn
-LEFT JOIN sb.al_items ON sb.al_items.bib_doc_num = sb.al_isbn.bib_doc_num
-LEFT JOIN sb.al_bib_fmt ON sb.al_bib_fmt.bib_doc_num = sb.al_isbn.bib_doc_num
+LEFT OUTER JOIN sb.al_items ON sb.al_items.bib_doc_num = sb.al_isbn.bib_doc_num
+LEFT OUTER JOIN sb.al_bib_fmt ON sb.al_bib_fmt.bib_doc_num = sb.al_isbn.bib_doc_num
 WHERE sb.al_isbn.scode = 'a'
 AND sb.al_items.inst = 'FS'
 AND (sb.al_items.sub_lib = 'FSUER' OR sb.al_items.sub_lib = 'FSOAR')
